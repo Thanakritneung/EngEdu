352ed914a2fc144fd7b9e3cc169b54ce082a35c3
58030171 <58030171@kmitl.ac.th>
2017-10-19 03:03:05 +0700

    1nd  year semester 1

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -13,6 +13,9 @@ namespace EngEdu
             Console.WriteLine("Year 255x semester 1, I am Engedu freshman.");
             Console.WriteLine("Year 255x semester 2, I learn to program in C sharp.");
             Console.WriteLine("Year 2560 semester 1, I start my programmer life.");
+
+            Console.WriteLine("Year 2558 semester 1, I learn to Laboratory,this's vary fun.");
+           
         }
     }
 }
